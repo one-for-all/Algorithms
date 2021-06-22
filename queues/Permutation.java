b9c8e06136777d3ec6f5fffe14e6482a7c757235
Jay <jn.sapiens@gmail.com>
2021-06-21 22:05:23 -0700

    pick k items from n items - O(k) memory

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -6,22 +6,32 @@
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
-        int k = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[0]); // Number of items to output
+        int n = 0; // Number of items we have obtained so far
         RandomizedQueue<String> queue = new RandomizedQueue<>();
         while (!StdIn.isEmpty()) {
             String value = StdIn.readString();
-            queue.enqueue(value);
+            n++;
+
+            if (n <= k) // Insert first k elements
+                queue.enqueue(value);
+            else {
+                // With probability k/n, replace an item in the queue with
+                // the new item
+                boolean insert = StdRandom.bernoulli((double) k / n);
+                if (insert) {
+                    queue.dequeue();
+                    queue.enqueue(value);
+                }
+            }
         }
 
-        int count = 0;
-        for (String val : queue) {
-            if (count >= k)
-                break;
+        assert queue.size() == k;
+        for (String val : queue)
             StdOut.println(val);
-            count++;
-        }
     }
 }
