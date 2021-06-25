8dcdfb3ed89da8a36c63c06be71b465e844802e8
Jay <jn.sapiens@gmail.com>
2021-06-24 22:18:40 -0700

    throw exception in constructor

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -9,9 +9,13 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    private static final double CONFIDENCE_95 = 1.96;
     private final double[] probs;
 
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException();
+
         probs = new double[trials];
         for (int i = 0; i < trials; i++) {
             Percolation perc = new Percolation(n);
@@ -37,20 +41,17 @@ public class PercolationStats {
     }
 
     public double confidenceLo() {
-        return mean() - 1.96 * stddev() / Math.sqrt(probs.length);
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(probs.length);
     }
 
     public double confidenceHi() {
-        return mean() + 1.96 * stddev() / Math.sqrt(probs.length);
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(probs.length);
     }
 
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
-        }
         PercolationStats percStats = new PercolationStats(n, trials);
         StdOut.println("mean                    = " + percStats.mean());
         StdOut.println("stddev                  = " + percStats.stddev());
